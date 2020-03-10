c6e8ab92fd6e1d580d1b45eff8431ee5f32d5be7
zyzx0079 <3085006676@qq.com>
2020-03-11 00:01:00 +0800

    6commit

--- src/main/java/maventest3/Test.java
+++ src/main/java/maventest3/Test.java
@@ -2,8 +2,13 @@ package maventest3;
 
 public class Test {
 
-	public static void main(String[] args) {
-		System.out.println(111);
+	private void fds(){
+		
+	
 	}
+	public static void main(String[] args) {
+		System.out.println(876543);
+	}
+	
 
 }
