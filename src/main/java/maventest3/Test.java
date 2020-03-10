ed42711fd0219ebf3cda40ba7839492b7088b995
zyzx0079 <3085006676@qq.com>
2020-03-11 00:25:54 +0800

    8commit

--- src/main/java/maventest3/Test.java
+++ src/main/java/maventest3/Test.java
@@ -2,14 +2,9 @@ package maventest3;
 
 public class Test {
 
-<<<<<<< HEAD
-	public static void main(String[] args) {
-		System.out.println(98765);
-=======
+
 	private void fds(){
 		
-	
->>>>>>> refs/heads/develop
 	}
 	public static void main(String[] args) {
 		System.out.println(876543);
